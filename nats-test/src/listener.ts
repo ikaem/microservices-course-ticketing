f1d5bbb2bba96212a22d8206033e870ad34dcd71
Karlo Marinovic <karlo.marinovic@init.hr>
2021-05-23 20:26:16 +0200

    added nats and tested

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,53 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('orders-service');
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
+});
+
+// stan.on('close', () => {
+//   console.log('NATS connection closed!');
+//   process.exit();
+// });
+
+process.on('SIGINT', () => {
+  console.log('sigint');
+  stan.close();
+});
+process.on('SIGTERM', () => {
+  console.log('i am not here');
+  stan.close();
+});
