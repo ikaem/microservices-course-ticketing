1fb6c158d04f14536c02b3644c9f0715d0591ede
Karlo Marinovic <karlo.marinovic@init.hr>
2021-05-25 19:04:54 +0200

    end of day

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,17 @@
+import { Message } from 'node-nats-streaming';
+import { Listener } from './base-listener';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './ticket-created-event';
+
+class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = 'payments-service';
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log('Event data here', data);
+
+    // if all is ok
+    msg.ack();
+  }
+}
+
+export { TicketCreatedListener };
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -15,33 +16,8 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('orders-service');
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'orders-service-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
-
-// stan.on('close', () => {
-//   console.log('NATS connection closed!');
-//   process.exit();
-// });
 
 process.on('SIGINT', () => {
   console.log('sigint');
