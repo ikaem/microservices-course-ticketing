e453c33b41acfdd20a132d06bfb603368d498628
Karlo Marinovic <karlo.marinovic@init.hr>
2021-07-15 20:18:56 +0200

    fixes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ import axios from 'axios';
 export const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.angrychaired.xyz',
       headers: req.headers,
     });
   } else {
